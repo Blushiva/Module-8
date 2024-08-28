c1dbdcb6f1b4893407240ca295ebaa84f54f9d4f
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-08-28 15:18:36 -0500

    Update Cli.ts
    updated markdown

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -6,9 +6,9 @@ import Motorbike from "./Motorbike.js";
 import Wheel from "./Wheel.js";
 
 // define the Cli class
-  // TODO: update the vehicles property to accept Truck and Motorbike objects as well
-  // TODO: You will need to use the Union operator to define additional types for the array
-  // TODO: See the AbleToTow interface for an example of how to use the Union operator
+  // updated the vehicles property to accept Truck and Motorbike objects as well
+  // used the Union operator to define additional types for the array
+  //See the AbleToTow interface for an example of how to use the Union operator
 
 class Cli {
   vehicles: (Car | Truck | Motorbike)[] = [];
